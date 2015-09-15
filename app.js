1abf3e275f7d1d1254d9f6f0ce570b99a013756e
Ryan Hall <ryan.hall0326@gmail.com>
2015-09-14 18:10:10 -0600

    Set up with jumbotron and attempt to parallax

---
+++ app.js
@@ -0,0 +1,32 @@
+var app = angular.module('PresApp', []);
+
+app.controller('MainController', ['$scope', function($scope)]) {
+
+$scope.topics = [
+{
+	budget: "Which solution do you feel would help balance out nation's budget?",
+	taxPolicy: "Which tax solution do you agree with?",
+	education: "Does the common core improve or hurt our education system?",
+	obamacare: "Good, bad, or good but needs changes?",
+	immigration: "Which solution do you think would solve our illegal immigration issues?",
+	abortion: "Pro-life or Free Choice?",
+	foreignPolicies: "Solution to ISIS, China, etc..?",
+	unemployment: "Solutions?",
+	gay/women rights: "Where do you stand?",
+	government/people power: "Do you prefer the government or people to have more power?",
+
+}];
+
+//jumbotron with parallax function
+var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+    parallax();
+});
+
+
+}]);
